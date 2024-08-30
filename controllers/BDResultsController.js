937ad5cb3833260f3715922bfbb07123bf87755f
Andrey Kostin <andrey.kostin.111@mail.ru>
2024-08-30 21:33:29 +0300

    [Feature] Autograde

--- controllers/BDResultsController.js
+++ controllers/BDResultsController.js
@@ -24,7 +24,7 @@ export const getSolutions = async (req, res) => {
             const taskDetails = await Tasks.findById(task.task_id);
             return {
               taskNumber: taskDetails ? taskDetails.taskNumber : "Удалено", // Используем "Unknown", если данные не найдены
-              firstUpdate: task.createdAt, // время создания задания
+              firstUpdate: task.doneAt, // время создания задания
               mark: task.mark, // оценка задания
               status: task.status,
               sonarStatus: task.sonarStatus,
@@ -450,7 +450,7 @@ export const makeTaskReport = async (req, res) => {
     doc.moveDown(27.2);
 
     const subject = `[ОЭ ${new Date().toLocaleDateString()}][${user._id}][${taskNumber}][SonarQube] Запрос поддержки от Admin`;
-    const body = `Добрый день!\n\nОписание проблемы: (опишите, что случилось)\nПриоритет: от 1 до 4 (1 - срочный, 4 - некритичный)\nЖелаемая дата окончания сопровождения: (проставьте желаемую дату разрешения вопроса)\n\n___\nС уважением,\nпользователь платформы проверки тестовых заданий\n${user.surname} ${user.name} ${user.patro}`;
+    const body = `Добрый день!\n\nОписание проблемы: (опишите, что случилось)\nПриоритет: от 1 до 4 (1 - срочный, 4 - некритичный)\nЖелаемая дата реакции: (проставьте желаемую дату разрешения Вашего запроса)\n\n___\nС уважением,\nпользователь платформы проверки тестовых заданий\n${user.surname} ${user.name} ${user.patro}`;
     const mailtoLink = `mailto:vcp-tech-sup@mail.ru?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
     doc.font("Montserrat-SemiBold").fontSize(14).fillColor('#c4c4c4')
